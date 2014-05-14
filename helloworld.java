dd7cf853a7076803b87a9d09a8cd4c43cc58f35d
Tokar Oleg <drfasser@gmail.com>
2014-05-14 23:03:36 +0300

    remove comments
    removed comments from the code

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,5 @@
-/**
- * The HelloWorldApp class implements an application that
- * simply prints "Hello World!" to standard output.
- */
 class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+        System.out.println("Hello World!");
     }
 }
